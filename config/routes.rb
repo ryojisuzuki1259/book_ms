1bc33d4f82dba8dc1753fb82a16fcae7c4da58f6
EC2 Default User <ec2-user@ip-172-31-94-166.ec2.internal>
2021-02-06 23:34:20 +0000

    Add routes, Add home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   devise_for :admins
+  
+  root "home#top"
+  
+  resources :end_users
+  get "end_users/select" => "end_users#select"
+  resources :books
+  resources :genres, only: [:index, :create]
+  resources :shelves, only: [:index, :create]
+  resources :cart_items, only: [:index, :create, :destroy]
+  delete "cart_items/destroy_all" => "cart_items#destroy_all"
+  resources :lends, only: [:index, :show, :create] do
+    patch "lend_details/:id" => "lend_details#update"
+  end
+  post "lends/confirm" => "lends#confirm"
+  resources :lend_details
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
