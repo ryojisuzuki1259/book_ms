3c9df23e77b330802108f0c6a86d12d71f09bc1a
EC2 Default User <ec2-user@ip-172-31-94-166.ec2.internal>
2021-02-07 03:01:46 +0000

    Remove admins_columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_020319) do
+ActiveRecord::Schema.define(version: 2021_02_07_025845) do
 
   create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
     t.integer "employee_number", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -29,7 +27,6 @@ ActiveRecord::Schema.define(version: 2021_02_07_020319) do
     t.string "address", null: false
     t.string "telephone_number", null: false
     t.date "birthday", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
