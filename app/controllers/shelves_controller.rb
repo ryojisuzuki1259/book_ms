d02e001da23afd3f8181455f614dd0d5b7523c1b
EC2 Default User <ec2-user@ip-172-31-94-166.ec2.internal>
2021-02-07 20:21:41 +0000

    Add shelf_feature

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,2 +1,28 @@
 class ShelvesController < ApplicationController
+    def index
+        @shelves = Shelf.all
+        @shelf = Shelf.new
+    end
+    
+    def create
+        @shelf = Shelf.new(shelf_params)
+        @shelf.save
+        redirect_to shelves_path
+    end
+    
+    def update
+        @shelf = Shelf.find(params[:id])
+        @shelf.update(shelf_is_active_params)
+        redirect_to shelves_path
+    end
+    
+    private
+    
+    def shelf_params
+        params.require(:shelf).permit(:name, :is_active)
+    end
+    
+    def shelf_is_active_params
+        params.require(:shelf).permit(:is_active)
+    end
 end
