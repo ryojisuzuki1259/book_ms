d9fb0bd401a78a84eafa23a185c540b83bc556de
EC2 Default User <ec2-user@ip-172-31-94-166.ec2.internal>
2021-02-07 20:25:29 +0000

    Add default_true to books_is_active_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_201711) do
+ActiveRecord::Schema.define(version: 2021_02_07_202314) do
 
   create_table "admins", force: :cascade do |t|
     t.string "encrypted_password", default: "", null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_201711) do
     t.integer "shelf_id", null: false
     t.string "title", null: false
     t.date "date_publication", null: false
-    t.boolean "is_active", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
