de27ed5bda1dafffa4c1d781c46d7585a4b2a0ed
EC2 Default User <ec2-user@ip-172-31-94-166.ec2.internal>
2021-02-07 02:36:32 +0000

    Add columns to admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    def after_sign_in_path_for(resource)
+        root_path(resource) # ログイン後に遷移するpathを設定
+    end
+    
+    protected
+    def configure_permitted_parameters #登録時に送信するカラムを追加
+       devise_parameter_sanitizer.permit(:sign_up, keys: [
+         :name, :organization, :prefecture, :introduction,
+       ])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_225118) do
+ActiveRecord::Schema.define(version: 2021_02_07_020319) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_02_06_225118) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "address", null: false
+    t.string "telephone_number", null: false
+    t.date "birthday", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
